531dc9ff24e75af875dc8e87aa519fc5842f11ca
Jezen5Volk <138075884+Jezen5Volk@users.noreply.github.com>
2023-07-02 12:36:17 -0700

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = 'my_python_package',
-    packages = ['my_python_package'],
-    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = '',
-    author_email = '',
-    url = 'github package source url',
-    download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
+    name = 'MCNeuronSim',
+    packages = ['MCNeuronSim'],
+    version = 'v1.0', 
+    description = 'Monte Carlo Neuron Simulation using the Kratky-Porod Wormlike Chain Model to simulate neuron geometry',
+    author = 'Samantha Herman',
+    author_email = 'dylanherman@ucsb.edu',
+    url = 'https://github.com/Jezen5Volk/MCNeuronSim',
+    download_url = 'https://github.com/Jezen5Volk/MCNeuronSim/archive/refs/tags/v1.0.tar.gz',
+    keywords = ['Neuron'],
     classifiers = [],
 )
